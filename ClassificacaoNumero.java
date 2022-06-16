93c03d10eaac1f8dea77e1ea63e95adf47006e8c
Pedro do Amaral Rangel <107447016+PedrimAr@users.noreply.github.com>
2022-06-15 23:29:40 -0300

    Create ClassificacaoNumero.java

---
+++ ClassificacaoNumero.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class ClassificacaoNumero {
+	
+	public static void main(String[] args) {
+		Scanner sc =  new Scanner(System.in);
+		System.out.println("Insira um número inteiro para ser verificado");
+		int num = sc.nextInt();
+		if(par(num) == true) {
+			System.out.println("\nO número " + num + " é par!");
+		}
+		else if(par(num) == false) {
+			System.out.println("\nO número " + num + " é ímpar!");
+		}
+		if(primo(num) == true) {
+			System.out.println("O número " + num + " é primo!");
+		}
+		else if(primo(num) == false) {
+			System.out.println("O número " + num + " não é primo!");
+		}
+	}
+	
+	public static boolean par(int num) {
+		if(num % 2 == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public static boolean primo(int num) {
+		int count = 0;
+		for(int i = 1; i <= num; i++) {
+				if(num % i == 0) {
+					count ++;
+				}
+		}
+		if(count == 2) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
